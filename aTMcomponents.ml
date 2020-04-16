3260f199c9013b18d2e8dddbc7e0ab0fda5930c6
mosesmayer <mosesmayer@gmail.com>
2020-04-16 23:50:02 +0700

    i/o stuff

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -33,10 +33,11 @@ type action =
 type account_spec = {name : string; id : id; balance : int} ;;
 
 (* initialize accts -- Establishes a database of accounts, each with a
-   name, aribtrary id, and balance. The names and balances are
+   name, arbitrary id, and balance. The names and balances are
    initialized as per the `accts` provided. *)
 val initialize : account_spec list -> unit ;;
 
+open Printf
 (*....................................................................
  Acquiring information from the customer
 *)
@@ -44,15 +45,38 @@ val initialize : account_spec list -> unit ;;
 (* acquire_id () -- Requests from the ATM customer and returns an id
    (akin to entering one's ATM card), by prompting for an id number
    and reading an id from stdin. *)
-val acquire_id : unit -> id ;;
+let rec acquire_id () : id =
+  printf "Enter id: ";
+    let tmp = int_of_string_opt (read_line ()) in
+    match tmp with
+    | None -> printf "Invalid id"; acquire_id ()
+    | Some x -> 
+      if List.exists (fun u -> u.id = x) !users then x
+      else (printf "Invalid id"; acquire_id ())
 
 (* acquire_amount () -- Requests from the ATM customer and returns an
    amount by prompting for an amount and reading an int from stdin. *)
-val acquire_amount : unit -> int ;;
+let rec acquire_amount () : int =
+  let x = int_of_string_opt (read_line ()) in
+    match x with
+    | Some y -> y
+    | None -> (printf "Invalid amount\n"; acquire_amount ())
+;;
 
 (* acquire_act () -- Requests from the user and returns an action to
    be performed, as a value of type action *)
-val acquire_act : unit -> action ;;
+let rec acquire_act () : action =
+  printf "Enter action: (B) Balance (-) Withdraw (+) Deposit (=) Done (X) Exit:";
+  let tmp = read_line () in
+  match tmp with
+  | "B" -> Balance
+  | "=" -> Next
+  | "X" -> Finished
+  | "-" | "+" ->
+    let value = acquire_amount () in
+    if tmp = "+" then Deposit value else Withdraw value
+  | "" | _ -> (printf "Invalid action.\n"; acquire_act ())
+;;
 
 (*....................................................................
   Querying and updating the account database
@@ -63,15 +87,21 @@ val acquire_act : unit -> action ;;
   
 (* get_balance id -- Returns the balance for the customer account with
    the given id. *)
-val get_balance : id -> int ;;
+let get_balance (i : id) : int =
+  let usr = List.find (fun x -> x.id = i) !users in
+  usr.id
 
 (* get_name id -- Returns the name associated with the customer
    account with the given id. *)
-val get_name : id -> string ;;
+let get_name (i : id) : string =
+  let usr = List.find (fun x -> x.id = i) !users in
+  usr.name
 
 (* update_balance id amount -- Modifies the balance of the customer
    account with the given id,setting it to the given amount. *)
-val update_balance : id -> int -> unit ;;
+let update_balance (i : id) (v : int) : unit =
+  let usr = List.find (fun x -> x.id = i) !users in
+  usr.balance := v;
 
 (*....................................................................
   Presenting information and cash to the customer
@@ -79,10 +109,14 @@ val update_balance : id -> int -> unit ;;
   
 (* present_message message -- Presents to the customer (on stdout) the
    given message followed by a newline. *)
-val present_message : string -> unit ;;
+let present_message (s : string) : unit =
+  printf "%s\n" s
+;;
 
 (* deliver_cash amount -- Dispenses the given amount of cash to the
    customer (really just prints to stdout a message to that
    effect). *)
-val deliver_cash : int -> unit ;;
+let deliver_cash (value : int) : unit =
+  printf "Here\'s your cash: %d\n" value
+;;
 
